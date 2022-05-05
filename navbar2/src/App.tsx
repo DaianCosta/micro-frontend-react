305d43d8566db9b0bb266d0b0f27c622dd2b0378
DaianCosta <desenvolvimentodaian@gmail.com>
2022-05-05 13:20:18 -0300

    first commit

---
+++ navbar2/src/App.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import * as singleSpa from "single-spa";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+export default function DisabledTabs() {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    singleSpa.navigateToUrl("/");
+  }, []);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+
+    switch (newValue) {
+      case 0:
+        singleSpa.navigateToUrl("dash");
+        break;
+      case 1:
+        singleSpa.navigateToUrl("app1");
+        break;
+      case 2:
+        singleSpa.navigateToUrl("app2");
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      aria-label="disabled tabs example"
+    >
+      <Tab label="Dash" />
+      <Tab label="App1" />
+      <Tab label="App2" />
+    </Tabs>
+  );
+}
---
+++ root/src/mfe-root-config.ts
@@ -0,0 +1,29 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@mfe/navbar2",
+  app: () => System.import("@mfe/navbar2"),
+  activeWhen: () => true,
+});
+
+registerApplication({
+  name: "@mfe/dashboard",
+  app: () => System.import("@mfe/dashboard"),
+  activeWhen: ["/dash"],
+});
+
+registerApplication({
+  name: "@mfe/app1",
+  app: () => System.import("@mfe/app1"),
+  activeWhen: ["/app1"],
+});
+
+registerApplication({
+  name: "@mfe/app2",
+  app: () => System.import("@mfe/app2"),
+  activeWhen: ["/app2"],
+});
+
+start({
+  urlRerouteOnly: true,
+});
